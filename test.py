3fd03494217e8e053598f1a4bcdab6855d2e6e66
Casper <casperbramm@hotmail.com>
2022-09-12 15:20:57 +0200

    mediapipe og opencv

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from pyedo import edo
+#mediapipe (for accesing external media on platform fx. access to pc camera)
+import mediapipe as mp
+#open cv (computervision):
+import cv2 as cv
 myedo = edo("""ip for wifi""")
-
-#hehehehe

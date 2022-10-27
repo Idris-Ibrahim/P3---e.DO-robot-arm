624cbe45f45fec75db515029b94e1e800fce9dc6
Casper <casperbramm@hotmail.com>
2022-10-27 17:01:38 +0200

    asdad

--- test.py
+++ test.py
@@ -7,22 +7,4 @@ import cv2 as cv
 import roslibpy as ros
 #pynput
 import pynput as pyn 
-
-import time
-import math
-from pyedo import edo
-
-import unittest
-import roslibpy
-
-myedo = edo('192.168.12.1')
-
-
-def StartUp(myedo):
-    myedo.init6Axes()
-    myedo.disengageStd()
-    myedo.moveJoints(-45, -45, 96, 17, 100, -11)
-
-
-
-print(StartUp(myedo))
+myedo = edo("""ip for wifi""")

0495a79c596ae79daadc85a027f955e5284e9927
Casper2910 <94049624+Casper2910@users.noreply.github.com>
2022-11-03 10:37:00 +0100

    test

--- BoxPack.py
+++ BoxPack.py
@@ -1,6 +1,21 @@
 from DefineBoxes import DefineBoxes
 from DefineItems import DefineItems
 
+ItemList = []
+BoxList = []
+
 DefineBoxes()
 DefineItems()
 
+print("Itemlist Length:", len(ItemList), "Boxlist Length:", len(BoxList))
+
+#først sammenligner vi item[0] volume og ser om det er mindre en box[0] volume
+
+j = 1
+i = 1
+
+if ItemList[j].volume > BoxList[i].volume:
+    print("fric")
+
+else : 
+    print("yes")
---
+++ ModulosExample.py
@@ -0,0 +1,7 @@
+import math
+
+a = 3.41323132
+
+b = a%1
+
+print(b)

76d4492b48f8c3ed7896954fe591c175da688eeb
Idris-Ibrahim <93320320+Idris-Ibrahim@users.noreply.github.com>
2022-12-01 15:15:30 +0100

    fis3

--- test.py
+++ test.py
@@ -1,23 +1,81 @@
-import cv2
+import cv2 as cv
+from cv2 import aruco
+import numpy as np
 
+calib_data_path = "D:/3/P3/P3-e.DO-robot-arm/MultiMatrix.npz"
 
-cap1 = cv2.VideoCapture(0)
-cap2 = cv2.VideoCapture(1)
+calib_data = np.load(calib_data_path)
+print(calib_data.files)
 
-while 1:
+cam_mat = calib_data["camMatrix"]
+dist_coef = calib_data["distCoef"]
+r_vectors = calib_data["rVector"]
+t_vectors = calib_data["tVector"]
 
-  ret1, img1 = cap1.read()
-  ret2, img2 = cap2.read()
+MARKER_SIZE = 8  # centimeters
 
-  if ret1 and ret2:
+marker_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 
-      cv2.imshow('img1',img1)
-      cv2.imshow('img2',img2)
+param_markers = aruco.DetectorParameters_create()
 
-      k = cv2.waitKey(100) 
-      if k == 27: #press Esc to exit
-         break
+cap = cv.VideoCapture(0)
 
-cap1.release()
-cap2.release()
-cv2.destroyAllWindows()
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    marker_corners, marker_IDs, reject = aruco.detectMarkers(
+        gray_frame, marker_dict, parameters=param_markers
+    )
+    if marker_corners:
+        rVec, tVec, _ = aruco.estimatePoseSingleMarkers(
+            marker_corners, MARKER_SIZE, cam_mat, dist_coef
+        )
+        total_markers = range(0, marker_IDs.size)
+        for ids, corners, i in zip(marker_IDs, marker_corners, total_markers):
+            cv.polylines(
+                frame, [corners.astype(np.int32)], True, (0, 255, 255), 4, cv.LINE_AA
+            )
+            corners = corners.reshape(4, 2)
+            corners = corners.astype(int)
+            top_right = corners[0].ravel()
+            top_left = corners[1].ravel()
+            bottom_right = corners[2].ravel()
+            bottom_left = corners[3].ravel()
+
+            # Since there was mistake in calculating the distance approach point-outed in the Video Tutorial's comment
+            # so I have rectified that mistake, I have test that out it increase the accuracy overall.
+            # Calculating the distance
+            distance = np.sqrt(
+                tVec[i][0][2] ** 2 + tVec[i][0][0] ** 2 + tVec[i][0][1] ** 2
+            )
+            # Draw the pose of the marker
+            point = cv.drawFrameAxes(frame, cam_mat, dist_coef, rVec[i], tVec[i], 4, 4)
+            cv.putText(
+                frame,
+                f"id: {ids[0]} Dist: {round(distance, 2)}",
+                top_right,
+                cv.FONT_HERSHEY_PLAIN,
+                1.3,
+                (0, 0, 255),
+                2,
+                cv.LINE_AA,
+            )
+            cv.putText(
+                frame,
+                f"x:{round(tVec[i][0][0],1)} y: {round(tVec[i][0][1],1)} ",
+                bottom_right,
+                cv.FONT_HERSHEY_PLAIN,
+                1.0,
+                (0, 0, 255),
+                2,
+                cv.LINE_AA,
+            )
+            # print(ids, "  ", corners)
+    cv.imshow("frame", frame)
+    key = cv.waitKey(1)
+    if key == 27:
+        break
+cap.release()
+cv.destroyAllWindows()

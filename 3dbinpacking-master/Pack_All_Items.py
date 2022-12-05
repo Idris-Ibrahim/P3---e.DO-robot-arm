e25011e0dea136fa7224bb1fdd3133e9828545df
Casper2910 <94049624+Casper2910@users.noreply.github.com>
2022-12-05 13:27:05 +0100

    Only by adding 1 bin will it work correctly atm

--- 3dbinpacking-master/Pack_All_Items.py
+++ 3dbinpacking-master/Pack_All_Items.py
@@ -2,64 +2,64 @@ from main import Packer, Bin, Item
 
 packer = Packer()
 
-packer.add_bin(Bin('small-envelope', 25, 30, 40, 300))
+packer.add_bin(Bin('Stor kasse', 50, 10, 20, 20))
 
-packer.add_item(Item('50g [powder 1]', 25, 30, 17, 1))
-packer.add_item(Item('50g [powder 1]', 25, 30, 17, 1))
-packer.add_item(Item('50g [powder 1]', 25, 30, 17, 1))
-packer.add_item(Item('50g [powder 1]', 25, 30, 17, 1))
-packer.add_item(Item('50g [powder 1]', 25, 30, 17, 1))
+packer.add_item(Item('Harddrive', 11.5, 1.5, 8, 1))
+packer.add_item(Item('PC Mouse', 7, 4.5, 12, 1))
+packer.add_item(Item('KeyBoard', 45, 4, 14, 1))
+packer.add_item(Item('Iphone', 8, 1.4, 15, 1))
+packer.add_item(Item('Headset', 26, 7, 22, 1))
 
-packer.pack()
 
-for b in packer.bins:
-    print(":::::::::::", b.string())
+while len(packer.items) > 0:
+    
+    print("________________________________________________________")
+    print("-------------------  OPEN NEW BOX  ---------------------")
+    print("________________________________________________________")
+    
+    packer.pack()
+    
+    control = 0
+    for b in packer.bins:
+        control = control + len(b.items)
+    
+    if control == 0:
+        print("no box could contain all kinds of item")
+        exit()
+   
+    for b in packer.bins:
+        print(":::::::::::", b.string())
 
-    print("FITTED ITEMS:")
-    for item in b.items:
-        print("====> ", item.string())
+        print("FITTED ITEMS:")
+        for item in b.items:
+            print("====> ", item.string(), "\n")
 
-    print("UNFITTED ITEMS:")
-    for item in b.unfitted_items:
-        print("====> ", item.string())
-
-    print("***************************************************")
-    print("***************************************************")
-
-print("________________________________________________________")
-print("________________________________________________________")
-print("________________________________________________________")
-print("________________________________________________________")
-
-# Deletes all items that has been packed from the bins.items array
-packer.items.clear()
-for box in packer.bins:
-    box.items.clear()
-
-# Adds all the items that couldn fit in the box the the array agian for retry without the fitted items    
-for box in packer.bins:
-    for item in box.unfitted_items:
-        packer.add_item(item)
-    box.unfitted_items.clear()
+        print("UNFITTED ITEMS:")
+        for item in b.unfitted_items:
+            print("====> ", item.string(), "\n")
         
+        #Iratible variable for counting empty space in the box    
+        TotalVolume = 0
+        for item in b.items:
+            ItemVolume = item.get_volume()
+            TotalVolume += ItemVolume
+        
+        BinVolume = b.get_volume()  
+        EmptySpace = BinVolume - TotalVolume
+        print("UNUSED VOLUME:")
+        print("====> ", EmptySpace, "\n")
+            
+    # Deletes all items that has been packed from the bins.items array
+    packer.items.clear()
+    for box in packer.bins:
+        box.items.clear()
 
-print("________________________________________________________")
-print("________________________________________________________")
-print("________________________________________________________")
-print("________________________________________________________")
-
-packer.pack()
-
-for b in packer.bins:
-    print(":::::::::::", b.string())
-
-    print("FITTED ITEMS:")
-    for item in b.items:
-        print("====> ", item.string())
-
-    print("UNFITTED ITEMS:")
-    for item in b.unfitted_items:
-        print("====> ", item.string())
-
-    print("***************************************************")
-    print("***************************************************")
+    # Adds all the items that couldn fit in the box the the array agian for retry without the fitted items    
+    for box in packer.bins:
+        for item in box.unfitted_items:
+            # Resets rotationtype
+            # Rotation type has to be reset, otherwise the algorithm will only try for the last rotation type with future boxes
+            item.rotation_type = 0
+            packer.add_item(item)
+            
+    box.unfitted_items.clear()

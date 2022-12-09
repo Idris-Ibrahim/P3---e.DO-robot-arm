108550435210bd9f2f1fec8ca338b7f0bce12f82
Casper <casperbramm@hotmail.com>
2022-12-09 16:46:51 +0100

    Future works: Skal kunne definere umulige items

--- 3dbinpacking-master/example_pack_all.py
+++ 3dbinpacking-master/example_pack_all.py
@@ -6,12 +6,16 @@ packer = Packer()
 
 packer.add_bin(Bin('SMALL BIN', 10, 10, 12, 300))
 
-packer.add_bin(Bin('BIG BIN', 100, 300, 300, 300))
+packer.add_bin(Bin('BIG BIN', 100, 100, 100, 300))
+
+packer.add_bin(Bin('BIGGEST BIN', 100, 100, 200, 300))
 
 
 packer.add_item(Item('Fugl', 10, 10, 7, 1))
 
-packer.add_item(Item('Mus', 8, 10, 12, 1))
+packer.add_item(Item('Hund', 20, 10, 20, 1))
+
+packer.add_item(Item('Mus', 8, 5, 12, 1))
 
 packer.add_item(Item('Elefant', 50, 100, 200, 1))
 

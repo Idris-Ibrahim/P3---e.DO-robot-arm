9d8b4a07e68a3243aef4ab5e7008cf6a29c710b4
Idris-Ibrahim <93320320+Idris-Ibrahim@users.noreply.github.com>
2022-12-20 14:45:32 +0100

    blablab

--- 3dbinpacking-master/example_pack_all.py
+++ 3dbinpacking-master/example_pack_all.py
@@ -1,7 +1,11 @@
 from main import Packer, Bin, Item
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
 
 # IMPORTING THE PACKER OBJECT
 packer = Packer()
+
 
 # FUNCTION FOR CLEARING ALL DATA IN PAKCER OBJECT
 def clear_packer(pack):
@@ -45,3 +49,47 @@ add()
 print("\n PACK ALL RANDOM: \n")
 packer.pack_all_items_random()
 print("\n ^ PACK ALL RANDOM: ^\n")
+
+
+# Define the dimensions of the bins and items
+bins = [(10, 10, 12), (20, 10, 12), (30, 15, 20), (50, 30, 40), (100, 50, 100)]
+items = [(10, 10, 7), (20, 10, 20), (8, 5, 12), (8, 5, 12), (5, 4, 15), (10, 4, 4)]
+
+# Iterate over the bins
+for i, bin in enumerate(bins):
+    # Create a figure and an axis
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plot the bin as a box
+    verts = [
+        (0, 0, 0),
+        (0, bin[1], 0),
+        (bin[0], bin[1], 0),
+        (bin[0], 0, 0),
+        (0, 0, bin[2]),
+        (0, bin[1], bin[2]),
+        (bin[0], bin[1], bin[2]),
+        (bin[0], 0, bin[2]),
+    ]
+    ax.add_collection(Poly3DCollection([verts], alpha=.25, facecolor='blue'))
+
+    # Plot the items as boxes inside the bin
+    for item in items:
+        verts = [
+            (0, 0, 0),
+            (0, item[1], 0),
+            (item[0], item[1], 0),
+            (item[0], 0, 0),
+            (0, 0, item[2]),
+            (0, item[1], item[2]),
+            (item[0], item[1], item[2]),
+            (item[0], 0, item[2]),
+        ]
+        ax.add_collection(Poly3DCollection([verts], alpha=.25, facecolor='red'))
+        ax.set_xlim(0, bin[0])
+        ax.set_ylim(0, bin[1])
+        ax.set_zlim(0, bin[2])
+
+    # Show the plot
+    plt.show()
